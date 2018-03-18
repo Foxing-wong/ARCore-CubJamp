cf65a9103f143ccc9402f0d1fd47a175c4006b6f
Lucky <1343140096@qq.com>
2018-03-19 00:17:48 +0800

    提交代码

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    //方体
+    public GameObject cube;
+
+	// Update is called once per frame
+	void Update () {
+
+        foreach (var touch in Input.touches)
+        {
+            Shot(touch.position);
+            if (Input.touchCount > 1 && touch.phase == TouchPhase.Began)
+                addCude();
+        }
+	}
+    
+    //创建方体
+    void addCude()
+    {
+        GameObject.Instantiate(cube, transform.position + transform.forward * 0.3f, Random.rotation);
+    }
+
+    //旋转
+    void Shot(Vector2 screenPoint)
+    {
+        var ray = Camera.main.ScreenPointToRay(screenPoint);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray,out hitInfo))
+        {
+            hitInfo.rigidbody.AddForceAtPosition(ray.direction, hitInfo.point);
+        }
+    }
+
+
+}
